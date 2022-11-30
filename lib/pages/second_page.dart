4469d033b9222f5b39302fe30b2c2838d61371c5
visal-99 <sal061710313@gmail.com>
2022-11-30 13:52:31 +0700

    second sreen

--- lib/pages/second_page.dart
+++ lib/pages/second_page.dart
@@ -46,9 +46,299 @@ class _SecondPageState extends State<SecondPage> {
                   )
                   ),
                 ),
-                )
+                ),
+            SliverList(delegate: SliverChildListDelegate([
+              Container(
+                height:220,
+                child: GridView.count(
+                    mainAxisSpacing:0.4,
+                    crossAxisCount: 4,
+                    children: [
+                      Container(
+                     child: Column(
+                       children: [
+                         Container(
+                           width:60,
+                           height: 60,
+                           child: Icon(Icons.telegram,color: Colors.blue,size: 32,),
+                           decoration: BoxDecoration(
+                             color: Colors.black12,
+                             borderRadius: BorderRadius.circular(50)
+                           ),
+                         ),
+                         Padding(
+                           padding: const EdgeInsets.only(top: 8.0),
+                           child: Text("Send",
+                               style: TextStyle(
+                                 fontSize: 15,
+                                 fontWeight: FontWeight.bold
+                               ),
+                               softWrap: false,
+                               overflow: TextOverflow.ellipsis),
+                         )
+                       ],
+                     ),
+                    ),
+                      Container(
+                        child: Column(
+                          children: [
+                            Container(
+                              width:60,
+                              height: 60,
+                              child: Icon(Icons.upcoming_outlined,color: Colors.blue,size: 32,),
+                              decoration: BoxDecoration(
+                                  color: Colors.black12,
+                                  borderRadius: BorderRadius.circular(50)
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(top: 8.0),
+                              child: Text("Request",
+                                  style: TextStyle(
+                                      fontSize: 15,
+                                      fontWeight: FontWeight.bold
+                                  ),
+                                  softWrap: false,
+                                  overflow: TextOverflow.ellipsis),
+                            )
+                          ],
+                        ),
+                      ),
+                      Container(
+                        child: Column(
+                          children: [
+                            Container(
+                              width:60,
+                              height: 60,
+                              child: Icon(Icons.drive_folder_upload,color: Colors.blue,size: 32,),
+                              decoration: BoxDecoration(
+                                  color: Colors.black12,
+                                  borderRadius: BorderRadius.circular(50)
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(top: 8.0),
+                              child: Text("Transfer",
+                                  style: TextStyle(
+                                      fontSize: 15,
+                                      fontWeight: FontWeight.bold
+                                  ),
+                                  softWrap: false,
+                                  overflow: TextOverflow.ellipsis),
+                            )
+                          ],
+                        ),
+                      ),
+                      Container(
+                        child: Column(
+                          children: [
+                            Container(
+                              width:60,
+                              height: 60,
+                              child: Icon(Icons.note_add_rounded,color: Colors.blue,size: 32,),
+                              decoration: BoxDecoration(
+                                  color: Colors.black12,
+                                  borderRadius: BorderRadius.circular(50)
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(top: 8.0),
+                              child: Text("Create",
+                                  style: TextStyle(
+                                      fontSize: 15,
+                                      fontWeight: FontWeight.bold
+                                  ),
+                                  softWrap: false,
+                                  overflow: TextOverflow.ellipsis),
+                            )
+                          ],
+                        ),
+                      ),
+
+                      Container(
+                        child: Column(
+                          children: [
+                            Container(
+                              width:60,
+                              height: 60,
+                              child: Icon(Icons.sticky_note_2_rounded,color: Colors.blue,size: 32,),
+                              decoration: BoxDecoration(
+                                  color: Colors.black12,
+                                  borderRadius: BorderRadius.circular(50)
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(left: 10.0,top: 8.0),
+                              child: Text("Send a message",
+                                  style: TextStyle(
+                                      fontSize: 15,
+                                      fontWeight: FontWeight.bold
+                                  ),
+                                  softWrap: false,
+                                  overflow: TextOverflow.ellipsis),
+                            )
+                          ],
+                        ),
+                      ),
+                      Container(
+                        child: Column(
+                          children: [
+                            Container(
+                              width:60,
+                              height: 60,
+                              child: Icon(Icons.payment_outlined,color: Colors.blue,size: 32,),
+                              decoration: BoxDecoration(
+                                  color: Colors.black12,
+                                  borderRadius: BorderRadius.circular(50)
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(top: 8.0),
+                              child: Text("Pay Bills",
+                                  style: TextStyle(
+                                      fontSize: 15,
+                                      fontWeight: FontWeight.bold
+                                  ),
+                                  softWrap: false,
+                                  overflow: TextOverflow.ellipsis),
+                            )
+                          ],
+                        ),
+                      ),
+                      Container(
+                        child: Column(
+                          children: [
+                            Container(
+                              width:60,
+                              height: 60,
+                              child: Icon(Icons.splitscreen_rounded,color: Colors.blue,size: 32,),
+                              decoration: BoxDecoration(
+                                  color: Colors.black12,
+                                  borderRadius: BorderRadius.circular(50)
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(top: 8.0),
+                              child: Text("Split the",
+                                  style: TextStyle(
+                                      fontSize: 15,
+                                      fontWeight: FontWeight.bold
+                                  ),
+                                  softWrap: false,
+                                  overflow: TextOverflow.ellipsis),
+                            )
+                          ],
+                        ),
+                      ),
+                      Container(
+                        child: Column(
+                          children: [
+                            Container(
+                              width:60,
+                              height: 60,
+                              child: Icon(Icons.group,color: Colors.blue,size: 32,),
+                              decoration: BoxDecoration(
+                                  color: Colors.black12,
+                                  borderRadius: BorderRadius.circular(50)
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(left: 10.0,top: 8.0),
+                              child: Text("Share Contact",
+                                  style: TextStyle(
+                                      fontSize: 15,
+                                      fontWeight: FontWeight.bold
+                                  ),
+                                  softWrap: false,
+                                  overflow: TextOverflow.ellipsis),
+                            )
+                          ],
+                        ),
+                      ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 15,right: 15),
+                child: Divider(
+                    thickness: 1,
+                ),
+              ),
+              Container(
+                //color: Colors.orange,
+                padding: EdgeInsets.only(top: 0),
+                height: 280,
+                child: ListView(
+                  children: [
+                    ListTile(
+                      title: Text("Recently Activity",style: TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20
+                      ),),
+                      trailing: IconButton(icon: Icon(Icons.keyboard_tab,color: Colors.blue,),onPressed: (){},),
+                    ),
+                    ListTile(
+                      title: Text("Leslie Alexander",style: TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20
+                      ),),
+                      subtitle: Text("Preapproved Payment - Completed"),
+                      trailing: Text("+ \$129.5",style: TextStyle(
+                        color: Colors.green,
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold
+                      ),)
+                    ),
+                    ListTile(
+                        title: Text("Sovannah Nguyen",style: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20
+                        ),),
+                        subtitle: Text("Transfer Bank - Completed"),
+                        trailing: Text("- \$47.5",style: TextStyle(
+                            color: Colors.red,
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold
+                        ),)
+                    ),
+                    ListTile(
+                        title: Text("Cameron Williamson",style: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20
+                        ),),
+                        subtitle: Text("Preapproved Payment - Completed"),
+                        trailing: Text("+ \$86.5",style: TextStyle(
+                            color: Colors.green,
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold
+                        ),)
+                    ),
+                    ListTile(
+                        title: Text("Dianne Russell",style: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20
+                        ),),
+                        subtitle: Text("Transfer Bank - Completed"),
+                        trailing: Text(" - \$66.5",style: TextStyle(
+                            color: Colors.red,
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold
+                        ),)
+                    ),
+
+
+                  ],
+                ),
+              ),
+            ]))
           ],
-        )
+        ),
+
       ),
     );
   }
